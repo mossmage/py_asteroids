7084d8dd98732af4b35986a6d6a941f5e2ad29bc
mossmage <FaUzChunky@gmail.com>
2025-12-11 14:55:03 -0330

    add ability to quit :skull_emote:

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+    log_state()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+    screen.fill("black")
+    pygame.display.flip()
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
+
+if __name__ == "__main__":
+    main()
